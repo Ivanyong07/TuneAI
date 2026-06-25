8952f62753f599ade6aeec9ca1cc7d7740416d56
Ivan Yong Pak Theng <ivanyong07@gmail.com>
2026-06-25 14:39:15 +0800

    Finish rest api

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('instrument/', views.InstrumentListsView.as_view(), name='instrument'),
     path('instrument/<str:name>/',
          views.InstrumentDataView.as_view(), name='instrument-data'),
+    path('scan/', views.ScanLogInstrument.as_view(), name='scan')
 ]
--- api/views.py
+++ api/views.py
@@ -4,6 +4,9 @@ from .models import Instrument, ScanLog
 from .serializers import InstrumentSerializers, ScanLogSerializers
 from rest_framework.response import Response
 from rest_framework import status
+import base64
+from PIL import Image
+import io
 
 
 class InstrumentListsView(APIView):
@@ -26,3 +29,46 @@ class InstrumentDataView(APIView):
 
         serializer = InstrumentSerializers(Instrument)
         return Response(serializer.data)
+
+
+class ScanLogInstrument(APIView):
+    def post(self, request):
+
+        if 'image_base64' not in request.data:
+            return Response({
+                'error': 'No image provided'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            b64 = request.data['image_base64']
+            if ',' in b64:
+
+                b64 = b64.split(",")[1]
+                image_data = base64.b64decode(b64)
+        except Exception:
+            return Response(
+                {'error': 'Invalid image'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        detected_name = 'guitar'
+        confidence = 0.67
+
+        try:
+            instrument = Instrument.objects.get(name=detected_name)
+        except Instrument.DoesNotExist:
+            return Response(
+                {'detected': False, 'message': 'Instrument comming soon'}
+            )
+
+        ScanLog.objects.create(
+            instrument=instrument,
+            confidence=confidence
+        )
+
+        serializer = InstrumentSerializers(instrument)
+        return Response(
+            {'detected': True,
+             'confidence': round(confidence * 100, 1),
+             'instrument': serializer.data
+             })
