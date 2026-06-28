2d90c260d828a6063e44785a3f686c92a838f2a9
Ivan Yong Pak Theng <ivanyong07@gmail.com>
2026-06-28 23:17:16 +0800

    Successfuly show the guitar model

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,14 +1,41 @@
+const modelMap = {
+    'guitar':'guitar.glb',
+    'piano':'piano.glb',
+    'guzheng':'guzheng.glb',
+}
+
+function showModel(instrumentName){
+    const modelViewer = document.getElementById('instrument-model')
+    const modelContainer = document.getElementById('model-container')
+
+    const modelFile = modelMap[instrumentName]
+    if(modelFile){
+        modelContainer.classList.remove('hidden')
+        setTimeout(() => {
+            modelViewer.setAttribute('src', modelFile)
+        }, 100)
+        
+    }
+}
+
+function closeModel(){
+    document.getElementById('model-container').classList.add('hidden')
+    document.getElementById('instrument-model').setAttribute('src', '')
+}
+
+
 function handleResult(data) {
     const resultBox = document.getElementById('result-box')
     const instrumentName = document.getElementById('instrument-name')
     const confidenceText = document.getElementById('confidence-text')
     const statusText = document.getElementById('status-text')
 
-    if (data.detected) {
+    if (data.detected && data.confidence > 10) {
         resultBox.classList.remove('hidden')
         instrumentName.textContent = data.instrument.display_name
         confidenceText.textContent = `${data.confidence}% confidence`
         statusText.textContent = 'Instrument found!'
+        showModel(data.instrument.name)
     } else {
         resultBox.classList.add('hidden')
         statusText.textContent = 'Scanning...'
